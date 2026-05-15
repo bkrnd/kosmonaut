c58ec58b77a07e31dec8c067d99427ab9f229bad
bkrnd <boleslav.korenda@gmail.com>
2026-05-15 18:56:32 +0200

    Add simple WS example

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    experimental: {
+      websocket: true
+    }
+  },
+
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@vueuse/nuxt']
 })
