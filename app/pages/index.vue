72a1754d3d1ffe1157fe62372ac8f8c6a399bced
bkrnd <dev@bkrnd.cz>
2026-06-03 10:10:31 +0200

    refactor: restructure template layout and improve styling for main page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,24 +1,23 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <main>
-    <nav>
+  <div class="min-h-screen bg-[url(/images/background.png)] bg-contain">
+    <nav class="bg-gray-800 text-white px-8 py-4 flex items-center justify-between">
       <h1>My Chat App</h1>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
+      <ul class="flex space-x-4">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
       </ul>
     </nav>
-    <div>
-      <p>Welcome to the Chat App</p>
-      <div>Something</div>
-      <div>veery</div>
-      <div>important</div>
-    </div>
-    <div>
-      <Chat />
-    </div>
-  </main>
+    <main class="container mx-auto mt-8 grid gap-8">
+      <div class="p-4 bg-white">
+        <p>Welcome to the Chat App</p>
+        <div>Something</div>
+        <div>veery</div>
+        <div>important</div>
+      </div>
+      <div class="p-4 bg-white">
+        <Chat />
+      </div>
+    </main>
+  </div>
 </template>
